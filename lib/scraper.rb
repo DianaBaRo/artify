045adefb05d0db2c969ca452d6664a1b0c81bd66
Carolina <carolina@192.168.1.145>
2019-02-18 17:26:03 +0000

    css selectors

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -19,7 +19,8 @@ class Scraper
       exhibition_data[:venue] = exhibition.css("h5.tile-item--info---venue.venue").first.text
       exhibition_data[:starting_date] = exhibition.css("span.open-date").first.text
       exhibition_data[:closing_date] = exhibition.css("span.close-date").first.text
-      exhibition_data[:description] = exhibition.css(".tile-item--info p:nth-of-type(2)").first.text
+      exhibition_data[:individual_exhibition_url] = exhibition.css("h4.event-name a").attribute("href").value
+      #exhibition_data[:description] = exhibition.css(".tile-item--info p:nth-of-type(2)").first.text
 
       exhibitions << exhibition_data
     end
@@ -34,8 +35,7 @@ class Scraper
 
     exhibition = {}
 
-    exhibition[:title] = individual_exhibition.css("")
-    exhibition[:duration] = individual_exhibition.css("")
+    exhibition[:title] = individual_exhibition.css("h1.entry-title")
     exhibition[:times] = individual_exhibition.css("")
     exhibition[:cost] = individual_exhibition.css("")
     exhibition[:venue] = individual_exhibition.css("")
@@ -49,6 +49,7 @@ class Scraper
 end
 
 =begin
+
 link = 'http://www.artlyst.com/whats-on/'
   html = open(link)
   index = Nokogiri::HTML(html)
@@ -59,5 +60,22 @@ link = 'http://www.artlyst.com/whats-on/'
   starting_date = index.css("span.open-date").first.text
   closing_date = index.css("span.close-date").first.text
   description = index.css(".tile-item--info p:nth-of-type(2)").first.text
+  url = exhibition.css("h4.event-name a").attribute("href").value
   binding.pry
+
 =end
+
+  link = 'https://www.artlyst.com/whats-on-archive/young-gods/'
+  html = open(link)
+  individual_exhibition = Nokogiri::HTML(html)
+
+  name = individual_exhibition.css("h1.entry-title").text
+  times = individual_exhibition.css("")
+  binding.pry
+=begin
+  cost = individual_exhibition.css("")
+  address = individual_exhibition.css("")
+  contact = individual_exhibition.css("")
+  extended_description = individual_exhibition.css("")
+=end
+ 
