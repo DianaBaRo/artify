6bdad939e9bfff04869a038195a0fdbc3c83258c
Carolina <carolina@carolinas-air.mynet>
2019-02-18 14:52:29 +0000

    Scraping individual exhibition page

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,7 @@ require 'nokogiri'
 class Scraper
 
   def self.scrape_index_page(index_url)
-=begin
+
     html = open(index_url)
     index = Nokogiri::HTML(html)
 
@@ -25,14 +25,30 @@ class Scraper
     end
 
     exhibitions
-=end
+
   end
 
-  def self.scrape_profile_page(profile_url)
-    
+  def self.scrape_individual_exhibition_page(individual_exhibition_url)
+    html = open(profile_url)
+    individual_exhibition = Nokogiri::HTML(html)
+
+    exhibition = {}
+
+    exhibition[:title] = individual_exhibition.css("")
+    exhibition[:duration] = individual_exhibition.css("")
+    exhibition[:times] = individual_exhibition.css("")
+    exhibition[:cost] = individual_exhibition.css("")
+    exhibition[:venue] = individual_exhibition.css("")
+    exhibition[:address] = individual_exhibition.css("")
+    exhibition[:contact] = individual_exhibition.css("")
+    exhibition[:extended_description] = individual_exhibition.css("")
+
+    exhibition
+
   end
 end
 
+=begin
 link = 'http://www.artlyst.com/whats-on/'
   html = open(link)
   index = Nokogiri::HTML(html)
@@ -44,3 +60,4 @@ link = 'http://www.artlyst.com/whats-on/'
   closing_date = index.css("span.close-date").first.text
   description = index.css(".tile-item--info p:nth-of-type(2)").first.text
   binding.pry
+=end
