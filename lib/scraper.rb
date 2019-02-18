27bd8b11df72fb3e3dff4e845c998f1315578d79
Carolina <carolina@Carolinas-MacBook-Air.local>
2019-02-18 12:01:02 +0000

    css selector for exhibition index

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,8 +8,9 @@ require 'nokogiri'
 
   name = index.css("h4.event-name a").first.text
   venue = index.css("h5.tile-item--info---venue.venue").first.text
-  location = index.css()
-
+  starting_date = index.css("span.open-date").first.text
+  closing_date = index.css("span.close-date").first.text
+  description = index.css(".tile-item--info p:nth-of-type(2)").first.text
   binding.pry
 
 class Scraper
