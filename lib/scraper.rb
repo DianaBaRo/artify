bc6877e3c89ce26a83a8487f257625e26ee7b11d
Carolina <carolina@Carolinas-MacBook-Air.local>
2019-02-18 11:16:22 +0000

    Trying css selectors

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,6 +1,19 @@
-class Scraper
-  def self.scrape_index_page(index_url)
+require 'pry'
+require 'open-uri'
+require 'nokogiri'
 
+  link = 'http://www.artlyst.com/whats-on/'
+  html = open(link)
+  index = Nokogiri::HTML(html)
+
+  name = index.css("h4.event-name a").first.text
+
+  binding.pry
+
+class Scraper
+
+  def self.scrape_index_page(index_url)
+    
   end
 
   def self.scrape_profile_page(profile_url)
