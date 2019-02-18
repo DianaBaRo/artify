1a6a59aa23f178f21abb6ddf952e8e371426f3f0
Carolina <carolina@Carolinas-MacBook-Air.local>
2019-02-18 12:28:41 +0000

    Scraping index page

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,8 @@ require 'nokogiri'
   html = open(link)
   index = Nokogiri::HTML(html)
 
-  name = index.css("h4.event-name a").first.text
+  exhibition = index.css("div.tile-item.normal.Exhibition.recommended.editors_choice")
+  name = exhibition.css("h4.event-name a").first.text
   venue = index.css("h5.tile-item--info---venue.venue").first.text
   starting_date = index.css("span.open-date").first.text
   closing_date = index.css("span.close-date").first.text
@@ -16,7 +17,24 @@ require 'nokogiri'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+=begin
+    html = open(index_url)
+    index = Nokogiri::HTML(html)
+
+    exhibitions = []
+
+    individual_exhibition = index.css("div.tile-item.normal.Exhibition.recommended.editors_choice")
+
+    individual_exhibition.each do |exhibition|
+      exhibition_data = {}
+      exhibition_data[:name] = exhibition.css("h4.event-name a").first.text
+      exhibition_data[:venue] = exhibition.css("h5.tile-item--info---venue.venue").first.text
+      exhibition_data[:starting_date] = exhibition.css("span.open-date").first.text
+      exhibition_data[:closing_date] = exhibition.css("span.close-date").first.text
+      exhibition_data[:description] = exhibition.css(".tile-item--info p:nth-of-type(2)").first.text
+
+      students << student_data
+=end
   end
 
   def self.scrape_profile_page(profile_url)
