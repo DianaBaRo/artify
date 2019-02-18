a930b110c319c3da1bdb4e8f9537ecab7217f358
Carolina <carolina@Carolinas-MacBook-Air.local>
2019-02-18 12:31:25 +0000

    Adding scrape_index_page method

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,18 +2,6 @@ require 'pry'
 require 'open-uri'
 require 'nokogiri'
 
-  link = 'http://www.artlyst.com/whats-on/'
-  html = open(link)
-  index = Nokogiri::HTML(html)
-
-  exhibition = index.css("div.tile-item.normal.Exhibition.recommended.editors_choice")
-  name = exhibition.css("h4.event-name a").first.text
-  venue = index.css("h5.tile-item--info---venue.venue").first.text
-  starting_date = index.css("span.open-date").first.text
-  closing_date = index.css("span.close-date").first.text
-  description = index.css(".tile-item--info p:nth-of-type(2)").first.text
-  binding.pry
-
 class Scraper
 
   def self.scrape_index_page(index_url)
@@ -33,7 +21,10 @@ class Scraper
       exhibition_data[:closing_date] = exhibition.css("span.close-date").first.text
       exhibition_data[:description] = exhibition.css(".tile-item--info p:nth-of-type(2)").first.text
 
-      students << student_data
+      exhibitions << exhibition_data
+    end
+
+    exhibitions
 =end
   end
 
@@ -41,3 +32,15 @@ class Scraper
     
   end
 end
+
+link = 'http://www.artlyst.com/whats-on/'
+  html = open(link)
+  index = Nokogiri::HTML(html)
+
+  exhibition = index.css("div.tile-item.normal.Exhibition.recommended.editors_choice")
+  name = exhibition.css("h4.event-name a").first.text
+  venue = index.css("h5.tile-item--info---venue.venue").first.text
+  starting_date = index.css("span.open-date").first.text
+  closing_date = index.css("span.close-date").first.text
+  description = index.css(".tile-item--info p:nth-of-type(2)").first.text
+  binding.pry
