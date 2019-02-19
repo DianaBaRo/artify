37819fb303b15ccbc56d6cd7b617a722c01adde5
Carolina <carolina@carolinas-air.mynet>
2019-02-19 15:55:55 +0000

    All exhibition class methods

--- lib/exhibition.rb
+++ lib/exhibition.rb
@@ -2,6 +2,23 @@ class Exhibition
 
   attr_accessor :name, :venue, :starting_date, :closing_date, :exhibition_url, :description, :times, :cost, :address, :contact, :extended_description
 
-  
+  @@all = []
 
+  def initialize(exhibition_hash)
+    exhibition_hash.each {|k,v| self.send(("#{k}="),v)}
+    @@all << self
+  end
+
+  def self.create_from_collection(exhibitions_array)
+    exhibitions_array.each {|exhibition_hash| Exhibition.new(exhibition_hash)}
+  end 
+
+  def add_exhibition_attributes(attributes_hash)
+    attributes_hash.each{|k,v| self.send(("#{k}="),v)}
+    self
+  end 
+
+  def self.all
+    @@all
+  end
 end
