9ae336dd42792a21df46994c2294c52be990b987
Carolina <carolina@carolinas-air.mynet>
2019-02-19 15:48:39 +0000

    Renaming variables

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -19,7 +19,7 @@ class Scraper
       exhibition_data[:venue] = exhibition.css("h5.tile-item--info---venue.venue").first.text
       exhibition_data[:starting_date] = exhibition.css("span.open-date").first.text
       exhibition_data[:closing_date] = exhibition.css("span.close-date").first.text
-      exhibition_data[:individual_exhibition_url] = exhibition.css("h4.event-name a").attribute("href").value
+      exhibition_data[:exhibition_url] = exhibition.css("h4.event-name a").attribute("href").value
       exhibition_data[:description] = exhibition.css(".tile-item--info p:nth-of-type(2)").first.text
 
       exhibitions << exhibition_data
