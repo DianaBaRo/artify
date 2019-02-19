6c8452658b3c0ac74fc637e9cdc6a92bce8e4d00
Carolina <carolina@carolinas-air.mynet>
2019-02-19 15:49:02 +0000

    Adding attr accessor

--- lib/exhibition.rb
+++ lib/exhibition.rb
@@ -1,3 +1,7 @@
 class Exhibition
 
+  attr_accessor :name, :venue, :starting_date, :closing_date, :exhibition_url, :description, :times, :cost, :address, :contact, :extended_description
+
+  
+
 end
