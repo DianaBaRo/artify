4ea604d9b692caaf1992b7ef65fe88569e1aa458
Carolina <carolina@carolinas-air.mynet>
2019-02-20 11:19:39 +0000

    Requiring files inside lib

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,6 @@ Bundler.require(:default, :development)
 
 require 'open-uri'
 
-require_all 'lib'
+require_relative "../lib/cli.rb"
+require_relative "../lib/exhibition.rb"
+require_relative "../lib/scraper.rb"
