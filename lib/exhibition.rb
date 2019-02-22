1665cf2c97d25a8bfd72a7e64073fe1a5c964758
Carolina <carolina@carolinas-air.mynet>
2019-02-22 11:55:08 +0000

    Cleaning comments

--- lib/exhibition.rb
+++ lib/exhibition.rb
@@ -3,14 +3,13 @@ class Exhibition
   attr_accessor :name, :venue, :starting_date, :closing_date, :exhibition_url, :times, :cost, :address, :contact
 
   @@all = []
-#me gustaria method initialize vacio y añadir atributos de otra forma.
 
   def initialize(exhibition_hash)
     exhibition_hash.each {|k,v| self.send(("#{k}="),v)}
     @@all << self
   end
 
-  def self.create_from_collection(exhibitions_array) #Este array es el resultado de scrape_index_page
+  def self.create_from_collection(exhibitions_array)
     exhibitions_array.each {|exhibition_hash| Exhibition.new(exhibition_hash)}
   end 
 
